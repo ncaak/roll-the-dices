11bc6c5df3bb973ee43e0f4cc6f7bed14ca671ce
ncaak <ncaak@github.com>
2019-01-05 00:30:22 +0100

    Added logic to randomize rolls, Added Reply method to respond messages, Pending refactoring

--- lib/conn/conn.go
+++ lib/conn/conn.go
@@ -4,12 +4,16 @@ import(
 	"github.com/ncaak/roll-the-dices/structs/update"
 	"github.com/ncaak/roll-the-dices/lib/config"
 	"net/http"
+	"time"
 	"encoding/json"
+	"bytes"
 	"fmt"
 )
 
 func send(r *http.Request) *http.Response {
 	var client = &http.Client{}
+
+	client.Timeout = 30 * time.Second
 
 	resp, err := client.Do(r)
 	if err != nil {
@@ -35,4 +39,29 @@ func GetUpdates(offset int) []update.Result {
 	json.NewDecoder(resp.Body).Decode(&response) 
 
 	return response.Result
+}
+
+func SendReply(chatId int, msgText string, replyId int) {
+	var url = fmt.Sprintf("https://api.telegram.org/bot%s/sendMessage", config.GetToken())
+
+	type Message struct {
+		ChatId int `json:"chat_id"`
+		Text string `json:"text"`
+		ReplyId int `json:"reply_to_message_id"`
+	}
+
+
+	msg := Message{chatId, msgText, replyId}
+	jsonString, _ := json.Marshal(msg)
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonString))
+	if err != nil {
+		panic(err.Error())
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	var resp = send(req)
+	
+	defer resp.Body.Close()
 }
--- main.go
+++ main.go
@@ -5,6 +5,11 @@ import (
 	"github.com/ncaak/roll-the-dices/lib/storage"
 	"log"
 	"fmt"
+	"regexp"
+	"strconv"
+	"math/rand"
+	"time"
+	"strings"
 )
 
 func main() {
@@ -16,6 +21,58 @@ func main() {
 	for _, msg := range messages {
 		fmt.Println(msg)
 		fmt.Println(msg.IsCommand())
+
+		if msg.IsCommand() == true {
+			var cmd = msg.Message.Text
+			var rollCmd = regexp.MustCompile(`\/tira (.*)`).FindStringSubmatch(cmd)
+			var reply string
+			
+			if len(rollCmd) > 0 {
+				var roll = rollCmd[len(rollCmd)-1]
+				fmt.Println("tirada: ", roll)
+				var dicesRegex = regexp.MustCompile(`([+|-])?(\d)d(\d+)`)
+			//	var dice = regexp.MustCompile(`([+|-])?(\d)d(\d+)`)
+
+
+
+			//fmt.Println(dice.FindStringSubmatch(cmd))
+			//fmt.Println(dice.FindString(cmd)
+			var total = 0
+			for i, match := range dicesRegex.FindAllStringSubmatch(roll, -1) {
+				
+				var rollValues []string
+				fmt.Println(match, " encontrado ", i)
+
+				var dices, _ = strconv.Atoi(match[len(match)-2])
+				var faces, _ = strconv.Atoi(match[len(match)-1])
+				roll = strings.Replace(roll, match[0], "", -1)
+
+				rand.Seed(time.Now().Unix())
+				for rolls := 0; rolls < dices; rolls++ {
+					value := rand.Intn(faces) +1
+					fmt.Println("ha salido un", value)
+					total += value
+//					strValue _ := strconv.Itoa(value)
+					rollValues = append(rollValues, strconv.Itoa(value))
+				}
+
+				reply = fmt.Sprintf("%s %s%s", reply, match[0], rollValues)
+
+				fmt.Println("resto de la tirada: ", roll)
+			}
+			//fmt.Println(regexp.MustCompile(`\/tira (.*)`).FindStringSubmatch(cmd))
+				reply = fmt.Sprintf("%s= %d", reply, total)
+	//		fmt.Println("Resultados ", rollValues)
+				fmt.Println("Reply msg: ", reply)
+				fmt.Println("Para un total de ", total)
+
+			if roll != "" {
+				fmt.Println("/// quedan numeros que sumar")
+			}
+			
+			}
+			//conn.SendReply(msg.Message.From.Id, reply, msg.Message.MessageId)
+		}
 	}
 
 	storage.Close()
