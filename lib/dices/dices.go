8284f3a701c1c059d4d719e1f9f34ba2555120d8
ncaak <ncaak@github.com>
2019-05-28 12:16:32 +0200

    Reworked dices library with a more object approach

--- lib/dices/dices.go
+++ lib/dices/dices.go
@@ -2,151 +2,93 @@ package dices
 
 import (
 	"fmt"
+	"math/rand"
 	"regexp"
 	"strconv"
-	"math/rand"
-	"time"
 	"strings"
+	"time"
 )
 
-var reply string
-var roll string
-var total int
-
-func reset(command string) {
-	reply = ""
-	roll = command
-	total = 0
+type check struct {
+	dice int
+	faces int
+	results []int
 }
 
-func rollDices(dices int, faces int) []int {
-	var rollValues []int
-	rand.Seed(time.Now().UnixNano())
-	for rolls := 0; rolls < dices; rolls++ {
-		rollValues = append(rollValues, rand.Intn(faces)+1)
+type Roller struct {
+	command string
+	checks []check
+	bonus []int
+	total int
+}
+
+func Resolve(command string) (string, Roller) {
+	var r = Roller{command, []check{}, []int{},  0}
+
+	r.extractDice()
+
+	r.extractModifiers()
+
+	r.calcTotal()
+
+	return r.formatReply(), r
+}
+
+func (r *Roller) calcTotal () {
+	for _, check := range r.checks {
+		for _, result := range check.results {
+			r.total += result
+		}
 	}
-
-	return rollValues
 }
 
-func calcDices() {
-	var regexDices = regexp.MustCompile(`([+|-])?(\d)d(\d+)`)
+func (r *Roller) extractDice () {
+	var regexDices = regexp.MustCompile(`(\s?\+?\s?)?(\d)d(\d+)`)
 
-	for _, match := range regexDices.FindAllStringSubmatch(roll, -1) {
-		var rollValues []int
+	for _, match := range regexDices.FindAllStringSubmatch(r.command, -1) {
 		var dices, _ = strconv.Atoi(match[len(match)-2])
 		var faces, _ = strconv.Atoi(match[len(match)-1])
-		
-		rollValues = rollDices(dices, faces)
-		for _, value := range rollValues {
-			total += value
+		r.newCheck(dices, faces)
+		r.extractFromCommand(match[0])
+	}
+}
+
+func (r *Roller) extractModifiers () {
+	var regexBonus = regexp.MustCompile(`(\s?[+|-]\s?)(\d+)`)
+
+	for _, match := range regexBonus.FindAllStringSubmatch(r.command, -1) {
+		var strBonus = fmt.Sprintf("%s%s", strings.TrimSpace(match[1]), match[2])
+		var bonus, _ = strconv.Atoi(strBonus)
+		r.bonus = append(r.bonus, bonus)
+		r.extractFromCommand(match[0])
+	}
+}
+
+func (r *Roller) extractFromCommand (usedOrder string) {
+	r.command = strings.Replace(r.command, usedOrder, "", 1)
+}
+
+func (r *Roller) formatReply () string {
+	var fmtReply strings.Builder
+	for index, check := range r.checks {
+		var str = fmt.Sprintf("%dd%d%d", check.dice, check.faces, check.results)
+		if index > 0 {
+			fmtReply.WriteString("+")
 		}
-
-		reply = fmt.Sprintf("%s %s%d", reply, match[0], rollValues)
-		roll = strings.Replace(roll, match[0], "", 1)
+		fmtReply.WriteString(str)
 	}
+	
+	fmtReply.WriteString(fmt.Sprintf("= %d", r.total))
+
+	return fmtReply.String()
 }
 
-func calcBonus() {
-	var regexBonus = regexp.MustCompile(`([+|-])?(\d+)`)
-	
-	for _, match := range regexBonus.FindAllStringSubmatch(roll, -1) {
-		bonus, _ := strconv.Atoi(match[0])
-		total += bonus
-
-		reply = fmt.Sprintf("%s %s", reply, match[0])
-		roll = strings.Replace(roll, match[0], "", 1)
-	}
-}
-
-func tag() {
-	reply = fmt.Sprintf("%s: %s", strings.TrimSpace(roll), strings.TrimSpace(reply))
-}
-
-func Roll(command string) string {
-	//fmt.Println("roll: ", command)
-	if command != "" {
-		reset(command)
-	} else {
-		reset("1d20")
-	}
-
-	if roll != "" {
-		calcDices()
-	}
-
-	if roll != "" {
-		calcBonus()
-	}
-
-	if roll != "" {
-		tag()
-	}
-			
-	reply = fmt.Sprintf("%s = %d", reply, total)
-	
-	return reply
-}
-
-func Advantage(command string) string {
-	//fmt.Println("advantage: ", command)
-	var rollValues []int
-	
-	reset(command)
-	
-	rollValues = rollDices(2,20)	
-	if rollValues[0] > rollValues[1] {
-		total = rollValues[0]
-	} else {
-		total = rollValues[1]
-	}
-
-	reply = fmt.Sprintf("2d20%d", rollValues)
-
-	if roll != "" {
-		calcDices()
-	}
-	
-	if roll != "" {
-		calcBonus()
-	}
-
-	if roll != "" {
-		tag()
-	}
-
-	reply = fmt.Sprintf("%s = %d", reply, total)
-
-	return reply
-}
-
-func Disadvantage(command string) string {
-	//fmt.Println("disadvantage: ", command)
-	var rollValues []int
-	
-	reset(command)
-	
+func (r *Roller) newCheck (dice int, faces int) {
+	var ch = check{dice, faces, []int{}}
 	rand.Seed(time.Now().UnixNano())
-	for rolls := 0; rolls < 2; rolls++ {
-		rollValues = append(rollValues, rand.Intn(20)+1)
+	for rolls := 0; rolls < dice; rolls++ {
+		ch.results = append(ch.results, rand.Intn(faces)+1)
 	}
 
-	if rollValues[0] < rollValues[1] {
-		total = rollValues[0]
-	} else {
-		total = rollValues[1]
-	}
-	reply = fmt.Sprintf("2d20%d", rollValues)
-
-	if roll != "" {
-		calcBonus()
-	}
-
-	if roll != "" {
-		tag()
-	}
-
-	reply = fmt.Sprintf("%s = %d", reply, total)
-
-	return reply
+	r.checks = append(r.checks, ch)
 }
