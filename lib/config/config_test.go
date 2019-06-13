3a7df5cca5fe24c467a25783cc29e4faa1356ed4
ncaak <ncaak@github.com>
2019-06-13 18:21:45 +0200

    Added new (fake) unit test for configurationw

--- lib/config/config_test.go
+++ lib/config/config_test.go
@@ -12,5 +12,15 @@ func TestConfigBasic(t *testing.T) {
 	
 	var result = GetSettings(test)
 	t.Logf("Result: Token: %s, DbKey: %s", result.Token, result.Dbkey)
+	t.Logf("Result: %s", result)
 }
 
+func TestConfigDB(t *testing.T) {
+	var test = "ENV_DEV"
+	t.Logf("Test configuration for environment: %s", test)
+	t.Log("Expected result: 'Database: {Type:<type> User:<user> Pass:<pass> Name:<name> OffsetTable:<table> OffsetColumn:<column>}'")
+
+	var result = GetSettings(test)
+	t.Logf("Result: DataBase: %+v", result.DataBase)
+}
+	
