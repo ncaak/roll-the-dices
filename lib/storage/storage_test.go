7f8d0cd2e768e6ca3122b79d85ae90d378e1ea0d
ncaak <ncaak@github.com>
2019-06-14 13:09:13 +0200

    Added unit tests for storage package library

---
+++ lib/storage/storage_test.go
@@ -0,0 +1,33 @@
+package storage
+
+import (
+	"github.com/ncaak/roll-the-dices/lib/config"
+	"testing"
+)
+
+// Tests basic open and close database
+func TestStorageBasic(t *testing.T) {
+	var test = "ENV_DEV"
+	var settings = config.GetSettings("ENV_DEV")
+	t.Logf("Test database configuration for environment: %s", test)
+	var db = Init(settings.DataBase)
+	// Pings the opened database to check if everything was fine
+	if err := result.core.Ping(); err != nil {
+		t.Errorf("ERROR :: Cannot connect with database")
+	}
+	result.Close()
+	t.Logf("Result: Conexion with the database was correct")
+}
+
+// Tests basic open, retrieve a data, and close database afterwards
+func TestStorageRetrieve(t *testing.T) {
+	var test = "ENV_DEV"
+	var settings = config.GetSettings("ENV_DEV")
+	t.Logf("Test database configuration for environment: %s", test)
+	t.Log("Expected result: 'd1' type(d1) = int")
+	var db = Init(settings.DataBase)
+	// Retrieves offset value from the database
+	var result = db.GetOffset()
+	db.Close()
+	t.Logf("Result: Offset retrieved: %d", result)
+}
