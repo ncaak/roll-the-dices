e3f0be70d063efc9b1ba4d082bc081e1509b4bcb
ncaak <ncaak@github.com>
2019-06-17 17:50:06 +0200

    Fixed storage unit tests

--- lib/storage/storage_test.go
+++ lib/storage/storage_test.go
@@ -8,21 +8,21 @@ import (
 // Tests basic open and close database
 func TestStorageBasic(t *testing.T) {
 	var test = "ENV_DEV"
-	var settings = config.GetSettings("ENV_DEV")
+	var settings = config.GetSettings(test)
 	t.Logf("Test database configuration for environment: %s", test)
 	var db = Init(settings.DataBase)
 	// Pings the opened database to check if everything was fine
-	if err := result.core.Ping(); err != nil {
+	if err := db.core.Ping(); err != nil {
 		t.Errorf("ERROR :: Cannot connect with database")
 	}
-	result.Close()
+	db.Close()
 	t.Logf("Result: Conexion with the database was correct")
 }
 
 // Tests basic open, retrieve a data, and close database afterwards
 func TestStorageRetrieve(t *testing.T) {
 	var test = "ENV_DEV"
-	var settings = config.GetSettings("ENV_DEV")
+	var settings = config.GetSettings(test)
 	t.Logf("Test database configuration for environment: %s", test)
 	t.Log("Expected result: 'd1' type(d1) = int")
 	var db = Init(settings.DataBase)
