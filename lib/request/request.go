6759003ef7327960ffdad1265e523d5b1a37f67f
ncaak <ncaak@github.com>
2019-06-18 16:25:58 +0200

    Refactored GetUpdates function to separate request from parsing to an auxiliary function

--- lib/request/request.go
+++ lib/request/request.go
@@ -28,21 +28,20 @@ func Init(cfg config.API) api {
 // Sends a GET request to server to retrieve updates from the Offset
 // offset - Last retrieved update message Id
 func (api *api) GetUpdates(offset int) []structs.Result {
-	var endpoint = fmt.Sprintf("%s%s?offset=%d", api.url, "getUpdates", offset)
-
-	req, err := http.NewRequest("GET", endpoint, nil)
+	// Prepare the request to retrieve unreaded messages
+	req, err := http.NewRequest(
+		"GET",
+		fmt.Sprintf("%s%s?offset=%d", api.url, "getUpdates", offset),
+		nil,
+	)
 	if err != nil {
 		panic(err.Error())
 	}
-
+	// Handle the response and parse it to return modelled data (structs Update)
 	var resp = api.send(req)
-
 	defer resp.Body.Close()
 
-	response := structs.Update{}
-	json.NewDecoder(resp.Body).Decode(&response)
-
-	return response.Result
+	return getParsedUpdates(resp)
 }
 
 // Sends a POST request to server to deliver the reply to a message
@@ -72,6 +71,13 @@ func (api *api) send(r *http.Request) *http.Response {
 		panic(err.Error())
 	}
 	return resp
+}
+
+// Parse the response of GetUpdates request and model the data using Update structure
+func getParsedUpdates(response *http.Response) []structs.Result {
+	var updates = structs.Update{}
+	json.NewDecoder(response.Body).Decode(&updates)
+	return updates.Result
 }
 
 // Format the Reply using a struct and return the bytes object needed for the request
