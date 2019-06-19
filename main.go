4a082311021ef4eefd69c5ddfffd52bf14f9e6c3
ncaak <ncaak@github.com>
2019-06-19 15:01:07 +0200

    Refactored main function to handle rolls efficiently

--- main.go
+++ main.go
@@ -13,37 +13,29 @@ import (
 
 const ENVIRONMENT = "ENV_DEV"
 
-var acceptedCommands = [...]string{"tira", "v", "dv"}
-
 func main() {
 	log.Println("beginning routine")
 
 	var settings = config.GetSettings(ENVIRONMENT)
 	var db = storage.Init(settings.DataBase)
-	var http = request.Init(settings.Api)
-	var messages = http.GetUpdates(db.GetOffset())
+	var api = request.Init(settings.Api)
+	var messages = api.GetUpdates(db.GetOffset())
 
 	for _, msg := range messages {
 
 		if msg.IsCommand() == true {
-			var regexCommands = fmt.Sprintf("/(%s)(.*)", strings.Join(acceptedCommands[:], "|"))
-			var command = regexp.MustCompile(regexCommands).FindStringSubmatch(msg.GetCommand())
-			var roll dice.Roller
+			var command = regexp.MustCompile(`/(tira|v|dv)(.*)`).FindStringSubmatch(msg.GetCommand())
 
 			if len(command) > 0 {
 				var argument = strings.TrimSpace(command[2])
-				switch command[1] {
-				case acceptedCommands[0]: // tira
-					roll = dice.Resolve(argument, "1d20")
-
-				case acceptedCommands[1]: // v
-					roll = dice.Resolve(argument, "h2d20")
-
-				case acceptedCommands[2]: // dv
-					roll = dice.Resolve(argument, "l2d20")
+				var defaultValues = map[string]string{
+					"tira": "1d20",
+					"v":    "h2d20",
+					"dv":   "l2d20",
 				}
+				var roll = dice.Resolve(argument, defaultValues[command[1]])
 
-				http.SendReply(msg, roll.FormatReply())
+				api.SendReply(msg, roll.FormatReply())
 				fmt.Println("reply: ", roll.FormatReply())
 			}
 
